65f642394803f473b00ea7601fc752faa1123a53
Thongchai Subsaidee <chanoktrue@me.com>
2023-01-18 06:03:03 +0700

    Design

---
+++ GlassMorphismisCoolDemo/GlassMorphismisCoolDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassMorphismisCoolDemoApp.swift
+//  GlassMorphismisCoolDemo
+//
+//  Created by Thongchai Subsaidee on 18/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassMorphismisCoolDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
